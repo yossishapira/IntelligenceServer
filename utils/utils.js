046ef1608816c944d78ceb537a8498827c1a08ce
yossi shapira <yosi105009@gmail.com>
2025-12-24 17:08:17 +0200

    end validateUser

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,8 @@
 export const validateUser = async (req, res, next) => {
     try {
         const { username, password } = req.headers
-        const dataUsers = await readFile(pathUsers)
-        const foundName = dataUsers.find(user => user.username === username && user.password === parseInt(password))
-        console.log(foundName);
-        if (!foundName) {
-            return res.status(403).json("")
-        }
+        if(username === "yossi" && password === 123)
+            res.json({ok:true})
         next()
     } catch (err) {
         console.error(err)
